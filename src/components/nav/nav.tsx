d5ad02f3815512e65a9f15f517143e47491b56c2
diegolentz <diegoolentz@gmail.com>
2025-07-22 13:10:00 -0300

    home responsive

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,9 +1,17 @@
+import MenuIcon from '@mui/icons-material/Menu';
+import { Button } from '@mui/material';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { useRef, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import './nav.css';
-import { Link } from "react-router-dom";
-import { useRef } from "react";
+
 
 export const Nav = ({ withSearch, buscador }: { withSearch: boolean, buscador: (titulo: string) => Promise<void> }) => {
     const inputRef = useRef<HTMLInputElement>(null);
+        const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+        const open = Boolean(anchorEl);
+        const navigate = useNavigate();
 
     const handleSearch = () => {
         const titulo = (inputRef.current?.value || "").trim();
@@ -13,6 +21,17 @@ export const Nav = ({ withSearch, buscador }: { withSearch: boolean, buscador: (
     const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === "Enter") {
             handleSearch();
+        }
+    };
+
+        const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = (destino: string) => {
+        setAnchorEl(null);
+        if (destino) {
+            navigate(destino);
         }
     };
 
@@ -42,6 +61,79 @@ export const Nav = ({ withSearch, buscador }: { withSearch: boolean, buscador: (
                     <Link to="/movies">Peliculas</Link>
                     <Link to="/series">Series</Link>
                 </div>
+                   <div className="burger">
+                    <Button
+                        id="burger-button"
+                        onClick={handleClick}
+                        sx={{
+                             borderRadius: '1rem'
+                        }}
+                    >
+                        <MenuIcon
+                            sx={{
+                                fontSize: 30,
+                                color: 'var(--color-font)',
+                            }}
+                        />
+                    </Button>
+
+                    <Menu
+                        id="burger-menu"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={() => handleClose("")}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'right',
+                        }}
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        PaperProps={{
+                            sx: {
+                                width: '15rem',
+                                backdropFilter: 'blur(10px)',
+                                backgroundColor: 'var(--nav-color)',
+                                color: 'var(--color-font)',
+                                borderBottomLeftRadius: '10px',
+                                borderBottomRightRadius: '10px',
+                                borderTopLeftRadius: 0,
+                                borderTopRightRadius: 0,
+                                mt: 1.5,
+                                position: 'absolute',
+                                ml: 'auto',
+                                right: '0px',
+                                left: 'unset',
+                                border: 'var(--search-color) solid 1px',
+                                borderTop: 'none',
+                                zIndex: 2000,
+                            },
+                        }}
+                    >
+                        <MenuItem
+                            onClick={() => handleClose("/")}
+                            sx={{ fontSize: '1.5rem', fontWeight: 600 }}
+                        >
+                            Home
+                        </MenuItem>
+                        <MenuItem
+                            onClick={() => handleClose("/movies")}
+                            sx={{ fontSize: '1.5rem', fontWeight: 600 }}
+                        >
+                            Peliculas
+                        </MenuItem>
+                        <MenuItem
+                            onClick={() => handleClose("/series")}
+                            sx={{ fontSize: '1.5rem', fontWeight: 600 }}
+                        >
+                            Series
+                        </MenuItem>
+                        
+                      
+                    </Menu>
+                </div>
+
             </div>
         </nav>
     );
